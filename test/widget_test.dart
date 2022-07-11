0afc89716d64f431ffd398ca9256196053fe0738
leoSilvaWarren <leonardo.silva@warren.com.br>
2022-07-11 11:11:04 -0300

    refactor: move tests to own test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,13 +4,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_golden_test_alchemist/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  Future<void> loadWidget(WidgetTester tester) async {
+    const myApp = MyApp();
+    await tester.pumpWidget(myApp);
+  }
+
+  testWidgets('Verify that our counter starts at 0', (WidgetTester tester) async {
+    await loadWidget(tester);
     expect(find.text('0'), findsOneWidget);
+  });
+
+  testWidgets('Verify that our counter not starts at 1', (WidgetTester tester) async {
+    await loadWidget(tester);
     expect(find.text('1'), findsNothing);
+  });
+
+  testWidgets("Verify that our counter has't incremented.", (WidgetTester tester) async {
+    await loadWidget(tester);
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
     expect(find.text('0'), findsNothing);
+  });
+
+  testWidgets('Verify that our counter has incremented.', (WidgetTester tester) async {
+    await loadWidget(tester);
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
     expect(find.text('1'), findsOneWidget);
   });
+
 }
